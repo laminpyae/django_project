d705f88f9527ad53590d377f8fa9284a3cfbe229
laminpyae <laminpyae@gmail.com>
2023-03-25 07:18:03 +0630

    login part

--- django_level_five/learning_users/basic_app/views.py
+++ django_level_five/learning_users/basic_app/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from basic_app.forms import UserForm, UserProfileInfoForm
 
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def index(request):
     return render(request, 'basic_app/index.html')
+
+@login_required
+def special(request):
+    return HttpResponse("You are Logged In.")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
 def register(request):
     registered = False
@@ -36,3 +50,24 @@ def register(request):
                   {'user_form': user_form,
                    'profile_form': profile_form,
                    'registered': registered})
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+
+            else:
+                return HttpResponse("Account Not Active!")
+        else:
+            print("Someone tried to login and FAILED!")
+            print("Username: {} and password {}".format(username, password))
+            return HttpResponse('Invalid Login Details Supplied!')
+    else:
+        return render(request, 'basic_app/login.html', {})
--- django_level_five/learning_users/learning_users/settings.py
+++ django_level_five/learning_users/learning_users/settings.py
@@ -134,6 +134,8 @@ STATICFILES_DIRS = [STATEIC_DIR,]
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
+LOGIN_URL = '/basic_app/user_login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
